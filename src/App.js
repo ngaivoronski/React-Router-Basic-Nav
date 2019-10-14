3bbc0dfe9ea944be237d6bba3c7b2112235554b9
ngaivoronski <ngaivoronski@gmail.com>
2019-10-14 18:30:32 -0400

    mvp navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import { Route, Link } from "react-router-dom";
+import { Home, About, Contact} from './components';
 
 const App = () => (
   <div>
-    <Navigation />
+    <div className="App">
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Link to="/about">About</Link>
+      </div>
+      <div>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </div>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
